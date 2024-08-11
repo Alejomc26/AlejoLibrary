a2e4e7cfcf82877311d9458a713374167cda4e63
Alejomc26 <alechavezjaja26@gmail.com>
2024-08-11 14:12:40 -0500

    Hangar publish

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,8 @@
 import com.vanniktech.maven.publish.SonatypeHost
+import io.papermc.hangarpublishplugin.model.Platforms
 
 plugins {
+    id("io.papermc.hangar-publish-plugin") version "0.1.0"
     id("com.vanniktech.maven.publish") version "0.28.0"
     id("java")
 }
@@ -17,6 +19,24 @@ repositories {
 mavenPublishing {
     publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL, automaticRelease = false)
     signAllPublications()
+}
+
+hangarPublish {
+    publications.register("plugin") {
+        version.set(project.version as String)
+        channel.set("Snapshot")
+        id.set("hangar-project")
+        apiKey.set(System.getProperty("HANGAR_API_TOKEN"))
+        platforms {
+            register(Platforms.PAPER) {
+                jar.set(tasks.jar.flatMap { it.archiveFile })
+                val versions: List<String> = (property("paperVersion") as String)
+                    .split(",")
+                    .map { it.trim() }
+                platformVersions.set(versions)
+            }
+        }
+    }
 }
 
 dependencies {
